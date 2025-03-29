8d5765215e485cb28f7fb48efc757cf046b5665b
Okan Koca <b231210016@sakarya.edu.tr>
2025-03-29 14:26:47 +0300

    6.odev generic list ve nesne kullanımı

---
+++ 6.Odevler/GenericListVeNesne/GenericListVeNesne/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericListVeNesne
+{
+    class Car
+    {
+        public string Marka;
+        public string Model;
+        public double fuelConsumptionPerHundredKm;
+        public double totalDistance;
+
+        public double CalculateTotalFuelConsumption()
+        {
+            return (totalDistance / 100 ) * fuelConsumptionPerHundredKm;
+        }
+    }
+}
---
+++ 6.Odevler/GenericListVeNesne/GenericListVeNesne/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// list of cars
+using GenericListVeNesne;
+
+List<Car> cars = new List<Car>();
+cars.Add(
+    new Car { Marka = "Toyota", Model = "Corolla", fuelConsumptionPerHundredKm = 6.5, totalDistance = 2500 }
+);
+cars.Add(
+    new Car { Marka = "BMW", Model = "X5", fuelConsumptionPerHundredKm = 9.0, totalDistance = 1350 }
+);
+cars.Add(
+    new Car { Marka = "Mercedes", Model = "C200", fuelConsumptionPerHundredKm = 8.2, totalDistance = 1125}
+);
+
+for(int i= 0; i < cars.Count; i++)
+{
+    Console.WriteLine("Marka: " + cars[i].Marka + " " + "Model:" + cars[i].Model 
+        + " " + "Yaktığı yakıt: " + cars[i].fuelConsumptionPerHundredKm + " " +
+        " Toplam Yakıt Tüketimi: " + cars[i].CalculateTotalFuelConsumption());
+}
+
